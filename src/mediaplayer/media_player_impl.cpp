ed4f39faf45573ce24425a4b8992292aaa164c2d
edychang <echang.1983@gmail.com>
2014-10-14 17:50:40 +0800

    add media player implementation (based on qt4).

---
+++ src/mediaplayer/media_player_impl.cpp
@@ -0,0 +1,68 @@
+﻿//Ricinus, platform independent Digital Signage Player
+//Copyright (C) 2014 Edward Zhang
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "media_player_impl.h"
+
+RICINUS_NAMESPACE_USE;
+
+scene_impl::~scene_impl() {
+    for(size_t i = 0, n = widgets.size(); i < n; ++i) {
+        delete widgets.at(i);
+    }
+}
+
+void scene_impl::setup(const std::string &pid) {
+    id = pid;
+    // TODO
+}
+
+void scene_impl::cleanup() {
+    // TODO
+}
+
+bool scene_impl::prepared(std::string& id) const {
+    // TODO
+    return false;
+}
+
+void scene_impl::play() {
+    for(size_t i = 0, n = widgets.size(); i < n; ++i) {
+        static_cast<media_player_widget*> (widgets[i])->play();
+    }
+    is_playing = true;
+}
+
+void scene_impl::pause() {
+    for(size_t i = 0, n = widgets.size(); i < n; ++i) {
+        static_cast<media_player_widget*> (widgets[i])->pause();
+    }
+}
+
+void scene_impl::resume() {
+    for(size_t i = 0, n = widgets.size(); i < n; ++i) {
+        static_cast<media_player_widget*> (widgets[i])->resume();
+    }
+}
+
+void scene_impl::stop() {
+    for(size_t i = 0, n = widgets.size(); i < n; ++i) {
+        static_cast<media_player_widget*> (widgets[i])->stop();
+    }
+}
+
+bool scene_impl::playing() const {
+    return is_playing;
+}
---
+++ src/mediaplayer/media_player_impl.h
@@ -0,0 +1,54 @@
+﻿//Ricinus, platform independent Digital Signage Player
+//Copyright (C) 2014 Edward Zhang
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef __RICINUS_MEDIA_PLAYER_IMPL_H__
+#define __RICINUS_MEDIA_PLAYER_IMPL_H__
+
+#include "media_player.h"
+
+#include <vector>
+
+RICINUS_NAMESPACE_BEGIN
+
+class scene_impl : public scene {
+public:
+    scene_impl() : is_playing(false) {}
+    ~scene_impl();
+
+    void setup(const std::string &pid);
+    void cleanup();
+    bool prepared(std::string &id) const;
+
+    void play();
+    void pause();
+    void resume();
+    void stop();
+    bool playing() const;
+
+private:
+    /**
+     * @brief 媒体播放控件指针列表.
+     *
+     * @note 一份所有演职员名单。
+     */
+    std::vector<media_player_widget*> widgets;
+    std::string id;     ///< 当前准备播放或者正在播放的节目标示符
+    bool is_playing;       ///< 当前是否正在播放节目
+};
+
+RICINUS_NAMESPACE_END
+
+#endif // __RICINUS_MEDIA_PLAYER_IMPL_H__
